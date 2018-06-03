e22f428b9c81af1da82b2e11732a97b7161333af
Ernest Guevarra <ernest@validmeasures.org>
2018-06-03 20:50:09 +0100

    update data.R

--- R/data.R
+++ R/data.R
@@ -21,6 +21,45 @@ NULL
 
 ################################################################################
 #
+#' villages
+#'
+#' Villages dataset of Rwanda
+#'
+#' @format A data frame containing 12 columns and 14816 rows:
+#' \describe{
+#' \item{\code{Prov_ID}}{Province ID}
+#' \item{\code{Province}}{Name of Province}
+#' \item{\code{Dist_ID}}{District ID}
+#' \item{\code{District}}{Name of District}
+#' \item{\code{Sect_ID}}{Sector ID}
+#' \item{\code{Sector}}{Name of Sector}
+#' \item{\code{Cell_ID}}{Cell ID}
+#' \item{\code{Cell}}{Name of Cell}
+#' \item{\code{Vill_ID}}{Village ID}
+#' \item{\code{Village}}{Name of Village}
+#' \item{\code{Status}}{Whether village is rural or urban}
+#' \item{\code{FID}}{FID}
+#' }
+#'
+#' @source National Instituate of Statistics of Rwanda
+#'     \url{http://geodata-nisr-rwanda.opendata.arcgis.com}
+#'
 #
-#
+################################################################################
+"villages"
 
+
+################################################################################
+#
+#' cell_map
+#'
+#' Map dataset
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#
+################################################################################
