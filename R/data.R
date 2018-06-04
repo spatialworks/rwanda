dd5cc3cc6e16de306722f73626ef57bd683b06a1
Ernest Guevarra <ernest@validmeasures.org>
2018-06-04 13:04:55 +0100

    update data.R

--- R/data.R
+++ R/data.R
@@ -51,15 +51,96 @@ NULL
 
 ################################################################################
 #
-#' cell_map
+#' province_map
 #'
-#' Map dataset
+#' Aministrative level 1 (province) map of Rwanda
 #'
+#' @format A SpatialPolygonsDataFrame with 5 features:
+#' \describe{
+#'   \item{\code{OBJECTID}}{Unique row identifier}
+#'   \item{\code{Prov_ID}}{Unique province identifier}
+#'   \item{\code{Prov_Name}}{Name of province}
+#'   \item{\code{Province}}{Name of province (Kinyarwanda)}
+#' }
 #'
-#'
-#'
-#'
-#'
+#' @source National Instituate of Statistics of Rwanda
+#'     \url{http://geodata-nisr-rwanda.opendata.arcgis.com}
 #'
 #
 ################################################################################
+"province_map"
+
+
+################################################################################
+#
+#' district_map
+#'
+#' Aministrative level 2 (district) map of Rwanda
+#'
+#' @format A SpatialPolygonsDataFrame with 30 features:
+#' \describe{
+#'   \item{\code{OBJECTID}}{Unique row identifier}
+#'   \item{\code{Prov_ID}}{Unique province identifier}
+#'   \item{\code{Prov_Name}}{Name of province}
+#'   \item{\code{Dist_ID}}{Unique district identifier}
+#'   \item{\code{District}}{Name of district}
+#' }
+#'
+#' @source National Instituate of Statistics of Rwanda
+#'     \url{http://geodata-nisr-rwanda.opendata.arcgis.com}
+#'
+#
+################################################################################
+"district_map"
+
+
+################################################################################
+#
+#' sector_map
+#'
+#' Aministrative level 3 (sector) map of Rwanda
+#'
+#' @format A SpatialPolygonsDataFrame with 416 features:
+#' \describe{
+#'   \item{\code{OBJECTID}}{Unique row identifier}
+#'   \item{\code{Prov_ID}}{Unique province identifier}
+#'   \item{\code{Prov_Name}}{Name of province}
+#'   \item{\code{Dist_ID}}{Unique district identifier}
+#'   \item{\code{District}}{Name of district}
+#'   \item{\code{Sect_ID}}{Unique sector identifier}
+#'   \item{\code{Sector}}{Name of sector}
+#' }
+#'
+#' @source National Instituate of Statistics of Rwanda
+#'     \url{http://geodata-nisr-rwanda.opendata.arcgis.com}
+#'
+#
+################################################################################
+"sector_map"
+
+
+################################################################################
+#
+#' cell_map
+#'
+#' Aministrative level 4 (cell) map of Rwanda
+#'
+#' @format A SpatialPolygonsDataFrame with 2148 features:
+#' \describe{
+#'   \item{\code{OBJECTID}}{Unique row identifier}
+#'   \item{\code{Prov_ID}}{Unique province identifier}
+#'   \item{\code{Prov_Name}}{Name of province}
+#'   \item{\code{Dist_ID}}{Unique district identifier}
+#'   \item{\code{District}}{Name of district}
+#'   \item{\code{Sect_ID}}{Unique sector identifier}
+#'   \item{\code{Sector}}{Name of sector}
+#'   \item{\code{Cell_ID}}{Unique cell identifier}
+#'   \item{\code{Cell}}{Name of cell}
+#' }
+#'
+#' @source National Instituate of Statistics of Rwanda
+#'     \url{http://geodata-nisr-rwanda.opendata.arcgis.com}
+#'
+#
+################################################################################
+"cell_map"
